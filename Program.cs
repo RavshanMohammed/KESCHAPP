898209d13eea5e398c4832407230f5d866f52fad
RavshanMohammed <ravshanbekmuxamedov2001@gmail.com>
2022-11-29 14:15:41 +0500

    INFA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.WriteLine("Enter your name: ");
+
+string name = Console.ReadLine();
+string greeting = $"Hello {name} my name is Kescha(:";
+
+System.Console.WriteLine(greeting);
+
+System.Console.WriteLine("Enter your age: ");
+string ageAsString = Console.ReadLine();
+System.Console.WriteLine("Converting . . . ");
+int age = Convert.ToInt32(ageAsString);
+System.Console.WriteLine($"Successfully converted! {age}");
+int keschaAge = 13;
+int difference = age - keschaAge;
+System.Console.WriteLine($"The gap is {difference}");
+
+
+
+
+
+
+
+
+
